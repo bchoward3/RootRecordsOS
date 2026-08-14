34cae7b45ce90871b9759462364e6ef9ce08c25a
Bucky Badger <your_email@wisc.edu>
2026-08-14 14:40:30 -0400

    Adjusted sepia filter

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ const basemaps = {
 
 // Apply sepia CSS filter to map tiles
 const style = document.createElement('style');
-style.textContent = '.leaflet-tile-pane { filter: sepia(25%) brightness(60%) contrast(112%) saturate(80%); }';
+style.textContent = '.leaflet-tile-pane { filter: sepia(20%) brightness(80%) contrast(112%) saturate(80%); }';
 document.head.appendChild(style);
 
 basemaps.voyager.addTo(map);
