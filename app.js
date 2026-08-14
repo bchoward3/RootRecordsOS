5bf8e9c22886357b8b32b12daaa2beb7563a09c9
Bucky Badger <your_email@wisc.edu>
2026-08-14 14:25:35 -0400

    Adjusting sepia brightness

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ const basemaps = {
 
 // Apply sepia CSS filter to map tiles
 const style = document.createElement('style');
-style.textContent = '.leaflet-tile-pane { filter: sepia(25%) brightness(80%) contrast(112%) saturate(75%); }';
+style.textContent = '.leaflet-tile-pane { filter: sepia(25%) brightness(60%) contrast(112%) saturate(75%); }';
 document.head.appendChild(style);
 
 basemaps.voyager.addTo(map);
