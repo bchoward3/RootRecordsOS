70311afb914774078dbabbb4efc1809a7942e88c
Bucky Badger <your_email@wisc.edu>
2026-08-19 19:38:14 -0400

    Removed duplicate declaration

--- app.js
+++ app.js
@@ -360,9 +360,6 @@ let recordingTimer = null;
 let labelsVisible = false;
 let labelType = 'name';
 let mapLabelsLayer;
-let labelsActive = false;
-let labelType = 'name';
-let labelZoomThreshold = 12;
 
 // Image compression via canvas
 async function compressImage(file, maxWidth = 1200, quality = 0.8) {
