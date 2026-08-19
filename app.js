6b69c6f61b6b9f1a5b335b306b234b42aac390e2
Bucky Badger <your_email@wisc.edu>
2026-08-19 19:59:43 -0400

    Basemap update

--- app.js
+++ app.js
@@ -39,8 +39,8 @@ const basemaps = {
   satellite: L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
     attribution: '© Esri', maxZoom: 19
   }),
-  toner: L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}.png', {
-    attribution: '© Stadia Maps © Stamen Design © OpenStreetMap', maxZoom: 20
+  toner: L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    attribution: '© CartoDB © OpenStreetMap', maxZoom: 19
   }),
 };
 
