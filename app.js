f36e05c6335bf28b1a024bd6a0e937f80ef5ad8c
Bucky Badger <your_email@wisc.edu>
2026-08-19 20:03:27 -0400

    Basemap update again

--- app.js
+++ app.js
@@ -39,8 +39,8 @@ const basemaps = {
   satellite: L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
     attribution: '© Esri', maxZoom: 19
   }),
-  toner: L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-    attribution: '© CartoDB © OpenStreetMap', maxZoom: 19
+  toner: L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenTopoMap © OpenStreetMap', maxZoom: 17
   }),
 };
 
